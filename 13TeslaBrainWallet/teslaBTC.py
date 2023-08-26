3ee391670c8b10f99aa6c8bbdb8c38fd4ddc5383
0xBrenner <141441192+0xBrenner@users.noreply.github.com>
2023-08-26 03:04:47 -0700

    Update teslaBTC.py

--- 13TeslaBrainWallet/teslaBTC.py
+++ 13TeslaBrainWallet/teslaBTC.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
     
     phrase = str(sys.argv[1])
   
-    print "pass phrase: " + phrase
-    print "private key: " + private_key(phrase) 
-    print "BTC address: " + public_key(phrase) 
+    print( "pass phrase: " + phrase)
+    print( "private key: " + private_key(phrase))
+    print( "BTC address: " + public_key(phrase))
